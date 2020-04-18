950faf7faa8aaf528a5d933c747ae7fb84c71cf5
BuildTools <danielhands008@hotmail.co.uk>
2020-04-19 00:57:19 +0100

    Added basic shooting.

--- LD46-2020-Project/Assets/Scripts/PlayerControl.cs
+++ LD46-2020-Project/Assets/Scripts/PlayerControl.cs
@@ -6,19 +6,22 @@ public class PlayerControl : MonoBehaviour
 {
   public float speed;
 
+  public Transform Bullet;
+  public float ProjectileSpeed;
+
 
   private Rigidbody m_riigidbody;
   private Vector3 change;
 
   private Animator m_animator;
 
+  private float angle;
+
 
   void Start()
   {
     m_riigidbody = GetComponent<Rigidbody>();
     m_animator = GetComponent<Animator>();
-
-
   }
 
   private void Update()
@@ -31,11 +34,24 @@ public class PlayerControl : MonoBehaviour
     Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
 
     //Get the angle between the points
-    float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+    angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
 
     //Ta Daaa
     transform.rotation = Quaternion.Euler(new Vector3(-90f, -90f, -angle));
+Debug.DrawLine(transform.position, transform.up * 1000, Color.white);
+    if (Input.GetMouseButtonDown(0))
+    {
+      
+      Shoot();
+    }
 
+  }
+
+  void Shoot()
+  {
+    //Creating the bullet and shooting it
+    var pel = Instantiate(Bullet, transform.position, Quaternion.Euler(0, 0, angle));
+    pel.GetComponent<Rigidbody>().AddForce(transform.up * ProjectileSpeed);
   }
 
   float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
