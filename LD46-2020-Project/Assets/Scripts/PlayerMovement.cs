4a76be9f61109f56c59f7b7e000c7f0fee44458b
Scott Duff <duff.scott.richard@gmail.com>
2020-04-18 03:26:28 -0400

    Character Movement working but very much still a work in progress. lighting also somewhat fixed

--- LD46-2020-Project/Assets/Scripts/PlayerMovement.cs
+++ LD46-2020-Project/Assets/Scripts/PlayerMovement.cs
@@ -4,54 +4,50 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     public float speed;
 
-    private Rigidbody m_rigidbody;
+
+    private Rigidbody m_riigidbody;
     private Vector3 change;
 
-    //private Animator m_animator;
+    private Animator m_animator;
 
-    // Start is called before the first frame update
-    void Start()
+
+     void Start()
     {
-        m_rigidbody = GetComponent<Rigidbody>();
-        //m_animator = GetComponent<Animator>();
+        m_riigidbody = GetComponent<Rigidbody>();
+        m_animator = GetComponent<Animator>();
+
+
     }
 
+    private void Update()
+    {
+        
 
-    // Update is called once per frame
-    void Update()
+    }
+
+    private void FixedUpdate()
     {
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
-        change.y = Input.GetAxisRaw("Vertical");
-        //showing the float number of the getaxisraw input.
-        //Debug.Log(change);
+        change.z = Input.GetAxisRaw("Vertical");
 
-        //UpdateAnimationAndMove();
-      
+        MoveCharacter();
     }
-    //void UpdateAnimationAndMove()
-    //{
-    //    if (change != Vector3.zero)
-    //    {
-    //        MoveCharacter();
-    //        m_animator.SetFloat("moveX", change.x);
-    //        m_animator.SetFloat("moveY", change.y);
-    //        m_animator.SetBool("moving", true);
-    //    }
-    //    else
-    //    {
-    //        m_animator.SetBool("moving", false);
-    //    }
 
-    //}
 
     void MoveCharacter()
     {
-
-        m_rigidbody.MovePosition(transform.position + change * speed * Time.deltaTime);
-
+        m_riigidbody.MovePosition(transform.position + -change * speed * Time.deltaTime);
+        
     }
+
+
+
+
+
+
+
+
 }
