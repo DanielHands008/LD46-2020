267d08c0dcfcfee61e984864d7e7d35cf4ece0b6
Scott Duff <duff.scott.richard@gmail.com>
2020-04-18 01:47:37 -0400

    updated character

--- LD46-2020-Project/Assets/Scripts/Scottstestscript.cs
+++ LD46-2020-Project/Assets/Scripts/Scottstestscript.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DansTestScript : MonoBehaviour
+public class Scottstestscript : MonoBehaviour
 {
+    int thing = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class DansTestScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
