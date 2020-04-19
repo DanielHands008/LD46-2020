30b168f1492e13f398e4bc8fe8c2c1e72cffb9b0
BuildTools <danielhands008@hotmail.co.uk>
2020-04-19 04:58:57 +0100

    5AM

---
+++ LD46-2020-Project/Assets/Scripts/TaskBoard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskBoard : MonoBehaviour
+{
+
+public GameObject ConvoCanvas;
+  void OnTriggerEnter(Collider other)
+  {
+      if (other.gameObject.tag == "Player") {
+          ConvoCanvas.SetActive(true);
+          Debug.Log("Player has entered.");
+      }
+
+  }
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
